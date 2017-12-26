715028eb66f9a4c20b0bc861a82a787510e03f38
vektoreljavaali <vektoreljavaali@gmail.com>
2017-12-26 14:03:12 +0300

    ilk kurulum

---
+++ src/com/masaustu/main/mainForm.java
@@ -0,0 +1,43 @@
+package com.masaustu.main;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class mainForm {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainForm window = new mainForm();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public mainForm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
